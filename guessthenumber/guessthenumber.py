e0df99076a88bce153e830256dac7243e0417a26
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-05-04 13:22:14 +0700

    skill complete

--- guessthenumber/guessthenumber.py
+++ guessthenumber/guessthenumber.py
@@ -2,14 +2,46 @@
 import random
 import art
 
-print(art.guess_the_number)
-
+#global variables
 random_number = random.randint(1,100)
+turns = 0
 
-def home_screen(number):
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-    print(f"Psst, the correct answer is {number}")
-    input("Choose a dificulty. Type 'easy' or 'hard':")
+# home screen
+print(art.guess_the_number)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+#print(f"Psst, the correct answer is {random_number}")
+easy_or_hard = input("Choose a dificulty. Type 'easy' or 'hard':").lower()
 
-home_screen(random_number)
+# game logic
+if easy_or_hard == "easy":
+    turns = 10
+elif easy_or_hard == "hard":
+    turns = 5
+else:
+    print("you didn't select a valid difficulity")
+
+while turns > 0:
+    print(f"You have {turns} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    if guess < random_number:
+        print("Too low.")
+        turns -= 1
+        if turns == 0:
+            print("You've run out of guesses, you lose")
+            break
+        print("Guess agian")
+    if guess > random_number:
+        print("Too high.")
+        turns -= 1
+        if turns == 0:
+            print("You've run out of guesses, you lose")
+            break
+        print("Guess agian")
+    if guess == random_number:
+        print(f"You got it! The answer was {random_number}")
+        break
+
+
+
+
